6243ffd2fb1328b4bf5d33b0fa90e7fd5295f7a1
hyakt <hyakt0@gmail.com>
2020-02-07 01:32:38 +0900

    feature: styling modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -6,33 +6,71 @@ type Props = { }
 
 export const Modal: React.FC<Props> = props => {
 
-  const [display, setDisplay] = useState(true);
+  const [isDisplay, setIsDisplay] = useState(true);
 
-  const Modal = styled.
+  const Cover = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+  `;
 
+  const Container = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    height: 300px;
+    background: #fafafa;
+    border-radius: 3px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.3);
+  `;
+
+  const Title = styled.dt`
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: 18px;
+  `
+
+  const Button = styled.button`
+    background: ${(props: {primary?: boolean}) => props.primary ? "#4078c0" : "#bd2c00"};
+    color: #fafafa;
+    font-size: 1rem;
+    margin: 1rem;
+    width: 100px;
+    padding: 0.25em 1em;
+    border: none ;
+    border-radius: 3px;
+  `
 
   const handleOK = () => {
-    setDisplay(false)
+    setIsDisplay(false)
   }
 
   const handleCancel = () => {
-    setDisplay(false)
+    setIsDisplay(false)
   }
 
   return (
     createPortal(
-      display ?
-        <div data-testid='github-confirm-modal'>
-          <dl>
-            <dt>Are you sure</dt>
-            <dd>
-              <button onClick={handleOK}>OK</button>
-            </dd>
-            <dd>
-              <button onClick={handleCancel}>Cancel</button>
-            </dd>
-          </dl>
-        </div>
+      isDisplay ?
+        <Cover>
+          <Container data-testid='github-confirm-modal'>
+            <dl>
+              <Title>Are you sure?</Title>
+              <Button onClick={handleOK} primary>OK</Button>
+              <Button onClick={handleCancel}>Cancel</Button>
+            </dl>
+          </Container>
+        </Cover>
         : null,
       document.body
   ))
