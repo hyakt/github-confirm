1ea8e6831bac69656fc60d19843288e3bd838a6e
hyakt <hyakt0@gmail.com>
2020-02-08 11:34:30 +0900

    feature: on press modal

--- src/components/__tests__/modal.test.ts
+++ src/components/__tests__/modal.test.ts
@@ -1,26 +1,32 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
-import {render, cleanup, fireEvent} from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 
-import {Modal} from '../modal'
+import { Modal } from '../modal'
 
 describe('components/modal', () => {
   afterEach(cleanup);
   describe('when render the modal component', () => {
     it('create portal', () => {
-      const {container} = render(React.createElement(Modal))
+      const { container } = render(React.createElement(Modal))
       expect(container).toBeInTheDocument();
     })
-    it('destory portal when on click Ok button', () => {
-      const {getByText, queryByTestId} = render(React.createElement(Modal))
+    it('destory portal when click Ok button', () => {
+      const { getByText, queryByTestId } = render(React.createElement(Modal))
       expect(queryByTestId('github-confirm-modal')).not.toBeNull()
       fireEvent.click(getByText('OK'))
       expect(queryByTestId('github-confirm-modal')).toBeNull()
     })
-    it('destory portal when on click cancel button', () => {
-      const {getByText, queryByTestId} = render(React.createElement(Modal))
+    it('destory portal when click cancel button', () => {
+      const { getByText, queryByTestId } = render(React.createElement(Modal))
       expect(queryByTestId('github-confirm-modal')).not.toBeNull()
       fireEvent.click(getByText('Cancel'))
+      expect(queryByTestId('github-confirm-modal')).toBeNull()
+    })
+    it('destory portal when click cover', () => {
+      const { queryByTestId, getByTestId } = render(React.createElement(Modal))
+      expect(queryByTestId('github-confirm-modal')).not.toBeNull()
+      fireEvent.click(getByTestId('github-confirm-modal-cover'))
       expect(queryByTestId('github-confirm-modal')).toBeNull()
     })
   })
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,54 +2,58 @@ import React, { useState } from 'react'
 import { createPortal } from 'react-dom'
 import styled from 'styled-components'
 
-type Props = { }
+type Props = {}
+
+const Cover = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+`
+
+const Container = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 300px;
+  background: #fafafa;
+  border-radius: 3px;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.3);
+`
+
+const Title = styled.dt`
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 18px;
+`
+
+const Button = styled.button`
+  background: ${(props: { primary?: boolean }) => props.primary ? "#4078c0" : "#bd2c00"};
+  color: #fafafa;
+  font-size: 1rem;
+  margin: 1rem;
+  width: 100px;
+  padding: 0.25em 1em;
+  border: none ;
+  border-radius: 3px;
+`
 
 export const Modal: React.FC<Props> = props => {
 
-  const [isDisplay, setIsDisplay] = useState(true);
+  const [isDisplay, setIsDisplay] = useState(true)
 
-  const Cover = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.3);
-  `;
-
-  const Container = styled.div`
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 400px;
-    height: 300px;
-    background: #fafafa;
-    border-radius: 3px;
-    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.3);
-  `;
-
-  const Title = styled.dt`
-    text-align: center;
-    font-weight: bold;
-    margin-bottom: 18px;
-  `
-
-  const Button = styled.button`
-    background: ${(props: {primary?: boolean}) => props.primary ? "#4078c0" : "#bd2c00"};
-    color: #fafafa;
-    font-size: 1rem;
-    margin: 1rem;
-    width: 100px;
-    padding: 0.25em 1em;
-    border: none ;
-    border-radius: 3px;
-  `
+  const onClickCover = () => {
+    setIsDisplay(false)
+  };
 
   const handleOK = () => {
     setIsDisplay(false)
@@ -62,7 +66,7 @@ export const Modal: React.FC<Props> = props => {
   return (
     createPortal(
       isDisplay ?
-        <Cover>
+        <Cover data-testid='github-confirm-modal-cover' onClick={onClickCover}>
           <Container data-testid='github-confirm-modal'>
             <dl>
               <Title>Are you sure?</Title>
@@ -73,5 +77,5 @@ export const Modal: React.FC<Props> = props => {
         </Cover>
         : null,
       document.body
-  ))
+    ))
 }
