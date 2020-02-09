4d818b8f213cdcd930967a9db24fededaa7a2bc4
hyakt <hyakt0@gmail.com>
2020-02-09 23:57:00 +0900

    fix: event listener

--- src/confirm.ts
+++ src/confirm.ts
@@ -3,16 +3,27 @@ import { render } from 'react-dom'
 
 import { Modal } from './components/modal'
 
+
+const handleSubmit = (target: EventTarget | null, source: Element, event: Event): void => {
+  if (target === null) return;
+
+  if (target === source) {
+    render(React.createElement(Modal, { event: event }), document.createElement('div'))
+    event.preventDefault()
+    event.stopPropagation()
+  }
+}
+
 document.addEventListener('click', (event) => {
   const target = event.target
   const submitButtons = document.querySelectorAll('#partial-new-comment-form-actions button[type="submit"]')
+
   submitButtons.forEach(button => {
-    console.log('button: ', button);
-    if (target === button) {
-      console.log('submit!')
-      render(React.createElement(Modal, { event: event }), document.createElement('div'))
-      event.preventDefault()
-      event.stopImmediatePropagation()
+    if (button !== null) {
+      handleSubmit(target, button, event)
+      Array.from(button.children).forEach(child => {
+        handleSubmit(target, child, event)
+      })
     }
   })
 }, true)
